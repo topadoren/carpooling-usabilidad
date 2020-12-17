957d680d228df5a7d75dd09acb01957d24602100
topadoren <50046874+topadoren@users.noreply.github.com>
2020-12-17 00:30:05 -0300

    Update settings.py

--- DyAS/settings.py
+++ DyAS/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -136,3 +137,5 @@ LOGOUT_REDIRECT_URL = '/accounts/login'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+django_heroku.settings(locals())
